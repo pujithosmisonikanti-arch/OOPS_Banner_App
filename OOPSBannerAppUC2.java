e67d082d0878deea303e46c2b8e75603a9683814
techlearnlvn <131742101+techlearnlvn@users.noreply.github.com>
2026-03-03 23:13:17 +0530

    UC2: Display OOPS as 7-line ASCII banner using print statements

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Display "OOPS" as ASCII Banner using '*' characters
+ *
+ * @author Pujithosmi
+ * @version 2.0
+ */
+public class OOPSBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *****   ***** ");
+        System.out.println("*     *  *     *  *       *    ");
+        System.out.println("*     *  *     *  *       *     ");
+        System.out.println(" *****    *****   *       *      ");
+
+    }
+}
